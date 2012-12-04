0fcc5bb0af689d2a0b5a42c3866a38e663b74968
George Alonge <georgedelson@gmail.com>
2012-12-03 20:22:02 -0800

    initial commit of punkees

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Punkees::Application.config.secret_token = 'bed72d4c49e084411fa0dad6a486ac274fa8390b87fcdbdda2c92f4f8b482ef2090236fc4451dbdcd8a3610bd78fe21405b98e50061485a406d2f1e37a38bba6'
